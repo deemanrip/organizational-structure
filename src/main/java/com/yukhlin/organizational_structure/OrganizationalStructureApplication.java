d26df34675537e5b1354ffdf8071339c259c008a
Dmitry Yukhlin <deemansm@gmail.com>
2019-01-31 21:51:48 +0400

    Initial commit

---
+++ src/main/java/com/yukhlin/organizational_structure/OrganizationalStructureApplication.java
@@ -0,0 +1,14 @@
+package com.yukhlin.organizational_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationalStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizationalStructureApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/yukhlin/organizational_structure/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.yukhlin.organizational_structure;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OrganizationalStructureApplication.class);
+    }
+
+}
+
